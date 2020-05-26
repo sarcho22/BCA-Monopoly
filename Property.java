1fa4b1f122912dce73d341da34b28939553ccd91
alevol22 <alevol22@bergen.org>
2020-05-26 17:04:36 -0400

    try2

---
+++ Property.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Property here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Property extends Actor
+{
+    /**
+     * Act - do whatever the Property wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
